287f751e4b22a6f00ac9ef29f90a23bae8080303
Minh15062005 <minhlvph49143@gmail.com>
2024-11-18 15:13:29 +0700

    Add files via upload

--- sanpham.php
+++ sanpham.php
@@ -1,41 +1,71 @@
-<div class="row mb">
-    <div class="boxtrai mr">
-        <div class="row mb">
-            <div class="boxtitle"> SẢN PHẨM <strong><?= $tendm ?></strong></div>
-            <div class="row boxcontent">
-                <?php
-                $i = 0;
-                foreach ($dssp as $sp) {
-                    extract($sp);
-                    $linksp = "index.php?act=sanphamct&idsp=" . $id;
-                    $img_src = $img_path . $img;
+<?php
+function insert_sanpham($tensp, $giasp, $hinh, $mota, $iddm){
+    $sql = "INSERT INTO sanpham(name, price, img, mota, iddm) VALUES (?, ?, ?, ?, ?)";
+    pdo_execute($sql, $tensp, $giasp, $hinh, $mota, $iddm);
+}
 
-                    // Kiểm tra xem $price có phải là mảng không
-                    if (is_array($price)) {
-                        // Nếu $price là mảng, lấy giá trị đầu tiên hoặc xử lý phù hợp
-                        $price = $price[0]; // Hoặc bạn có thể xử lý giá trị khác trong mảng
-                    }
+function delete_sanpham($id){
+    $sql = "DELETE FROM sanpham WHERE id = ?";
+    pdo_execute($sql, $id);
+}
 
-                    // Set the class for margin adjustment
-                    if (($i % 3) == 2) {
-                        $class = "";
-                    } else {
-                        $class = "mr";
-                    }
+function loadall_sanpham_home(){
+    $sql = "SELECT * FROM sanpham ORDER BY id DESC LIMIT 0, 9";
+    return pdo_query($sql);
+}
 
-                    // In ra thông tin sản phẩm
-                    echo '<div class="boxsp ' . $class . '">
-                        <div class="row img"><a href="' . $linksp . '"><img src="' . $img_src . '" alt=""></a></div>
-                        <p>$' . number_format($price, 0, ',', '.') . '</p> <!-- Hiển thị giá với định dạng thích hợp -->
-                        <a href="' . $linksp . '">' . $name . '</a>
-                    </div>';
-                    $i += 1;
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-    <div class="boxphai">
-        <?php include "boxright.php"; ?>
-    </div>
-</div>
+function loadall_sanpham_top10(){
+    $sql = "SELECT * FROM sanpham ORDER BY luotxem DESC LIMIT 0, 10";
+    return pdo_query($sql);
+}
+
+function loadall_sanpham($kyw = "", $iddm = 0) {
+    $sql = "SELECT * FROM sanpham WHERE 1";
+    if ($kyw != "") {
+        $sql .= " AND name LIKE ?";
+        $kyw = "%".$kyw."%";
+    }
+    if ($iddm > 0) {
+        $sql .= " AND iddm = ?";
+    }
+    $sql .= " ORDER BY id DESC";
+    
+    // Tạo mảng chứa các tham số
+    $params = [];
+    if ($kyw != "") {
+        $params[] = $kyw;
+    }
+    if ($iddm > 0) {
+        $params[] = $iddm;
+    }
+
+    // Gọi hàm pdo_query với câu lệnh SQL và các tham số
+    return pdo_query($sql, ...$params);
+}
+
+
+function loadone_ten_dm($id) {
+    $sql = "SELECT * FROM sanpham WHERE id = ?";
+    return pdo_query_one($sql, $id);
+}
+
+function loadone_sanpham($id){
+    $sql = "SELECT * FROM sanpham WHERE id = ?";
+    return pdo_query_one($sql, $id);
+}
+
+function loadone_sanpham_cungloai($id, $iddm){
+    $sql = "SELECT * FROM sanpham WHERE iddm = ? AND id <> ?";
+    return pdo_query($sql, $iddm, $id);
+}
+
+function update_sanpham($id, $iddm, $tensp, $giasp, $mota, $hinh){
+    if ($hinh != "") {
+        $sql = "UPDATE sanpham SET iddm = ?, name = ?, price = ?, mota = ?, hinh = ? WHERE id = ?";
+        pdo_execute($sql, $iddm, $tensp, $giasp, $mota, $hinh, $id);
+    } else {
+        $sql = "UPDATE sanpham SET iddm = ?, name = ?, price = ?, mota = ? WHERE id = ?";
+        pdo_execute($sql, $iddm, $tensp, $giasp, $mota, $id);
+    }
+}
+?>
